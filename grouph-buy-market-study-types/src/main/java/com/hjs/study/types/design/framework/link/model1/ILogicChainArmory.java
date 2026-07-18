6bbf2d2fb49a73f7d7c68bf40acf8c29248655bf
huangjs <h2508268112@outlook.com>
2026-07-18 16:56:04 +0800

    feat： 责任链抽象模版设计

---
+++ grouph-buy-market-study-types/src/main/java/com/hjs/study/types/design/framework/link/model1/ILogicChainArmory.java
@@ -0,0 +1,7 @@
+package com.hjs.study.types.design.framework.link.model1;
+
+public interface ILogicChainArmory<T, D, R> {
+    ILogicLink<T, D, R> next();
+
+    ILogicLink<T, D, R> appendNext(ILogicLink<T, D, R> next);
+}
